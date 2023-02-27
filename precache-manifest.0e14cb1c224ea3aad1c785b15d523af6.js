b3b47ac410b0379df04c8df9998baa4eab74e248
phyya <phaboolous11@gmail.com>
2023-02-27 18:36:34 +0100

    Updates

---
+++ precache-manifest.0e14cb1c224ea3aad1c785b15d523af6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f3ae4efd05a52dfea3d",
+    "url": "/spefind/static/css/main.0d6eda62.chunk.css"
+  },
+  {
+    "revision": "8f3ae4efd05a52dfea3d",
+    "url": "/spefind/static/js/main.babd534b.chunk.js"
+  },
+  {
+    "revision": "e00478afb1c32508baf2",
+    "url": "/spefind/static/js/runtime~main.06d86c3e.js"
+  },
+  {
+    "revision": "99b1961c55d675e491f5",
+    "url": "/spefind/static/js/2.8808d590.chunk.js"
+  },
+  {
+    "revision": "fa398c7f156e2f3c0642de8ce4cb37c2",
+    "url": "/spefind/index.html"
+  }
+];
