529a9eccc0611dd133a7675038f47802ee410038
Ifechukwu <ifechiglory@gmail.com>
2023-03-11 12:38:51 +0100

    Explore page

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import './App.css';
 import SignIn from './Components/SignIn/SignIn';
 import SignUp from './Components/SignUp/SignUp';
 import CreateProfile from './pages/CreateProfile/CreateProfile';
-import Home from './pages/Home';
+import Home from './pages/Home'
+import Explore from './pages/Explore/Explore'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-	  <Route path='/signUp' element={<SignUp/>}/>
-	  <Route path='/login' element={<SignIn/>}/>
+	    <Route path='/signUp' element={<SignUp/>}/>
+	    <Route path='/login' element={<SignIn/>}/>
       <Route path="/createProfile" element={<CreateProfile />} />
+	    <Route path='/explore' element={<Explore/>}/>
     </Routes>
   );
 }
