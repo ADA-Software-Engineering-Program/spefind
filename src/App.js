319b6ba5365a092d169bdbce46f27da7662feb5e
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-03-13 19:22:50 +0100

    feat: built and styled the book me page
    Signed-off-by: Moyinoluwa Adelowo <moyinadelowo@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import SignIn from './Components/SignIn/SignIn';
-import SignUp from './Components/SignUp/SignUp';
-import CreateProfile from './pages/CreateProfile/CreateProfile';
-import Home from './pages/Home'
-import Explore from './pages/Explore/Explore'
-import Contact from './pages/Contact/Contact'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import SignIn from "./Components/SignIn/SignIn";
+import SignUp from "./Components/SignUp/SignUp";
+import CreateProfile from "./pages/CreateProfile/CreateProfile";
+import Home from "./pages/Home";
+import Explore from "./pages/Explore/Explore";
+import Contact from "./pages/Contact/Contact";
+import Bookme from "./pages/BookMe/Bookme";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-	    <Route path='/signUp' element={<SignUp/>}/>
-	    <Route path='/login' element={<SignIn/>}/>
+      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/login" element={<SignIn />} />
       <Route path="/createProfile" element={<CreateProfile />} />
-	    <Route path='/explore' element={<Explore/>}/>
-      <Route path='/contact' element={<Contact/>}/>
+      <Route path="/explore" element={<Explore />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/bookme" element={<Bookme />} />
     </Routes>
   );
 }
 
 export default App;
-
